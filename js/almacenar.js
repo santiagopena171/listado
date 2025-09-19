ad33f7348c36d3fe9e742feb7bd696f1b54debef
santiagopena171 <santijoepena@gmail.com>
2025-09-19 18:52:26 -0300

    tarea

--- js/almacenar.js
+++ js/almacenar.js
@@ -4,6 +4,15 @@ const input = document.getElementById('item');
 const contenedor = document.getElementById('contenedor');
 let listado = [];
 
+if (localStorage.getItem('listado')) {
+    try {
+        listado = JSON.parse(localStorage.getItem('listado')) || [];
+    } catch (e) {
+        listado = [];
+    }
+}
+const limpiar = document.getElementById('limpiar');
+
 function actualizarVista() {
     contenedor.innerHTML = '';
     listado.forEach(function(item) {
@@ -14,13 +23,22 @@ function actualizarVista() {
     });
 }
 
+actualizarVista();
+
 agregar.addEventListener('click', function() {
     const texto = input.value.trim();
     if (texto !== '') {
         listado.push(texto);
+        localStorage.setItem('listado', JSON.stringify(listado));
         actualizarVista();
         input.value = '';
     }
+});
+
+limpiar.addEventListener('click', function() {
+    listado = [];
+    localStorage.setItem('listado', JSON.stringify(listado));
+    actualizarVista();
 });
 
    
